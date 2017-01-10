a9c679a96e24690c1174e0b2ee33fc9c611d35c4
Qian Chen <oda_san@hotmail.com>
2017-01-09 18:12:35 -0800

    lesson

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 var express = require('express');
 var fs = require('fs');
+var morgan = require('morgan');
+var favicon = require('serve-favicon');
 
 var app = express();
+
+// mounting middleware
+app.use(morgan('combined'));
+
+// use favicon
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.use(function (req, res, next) {
+  console.log('1 Incoming request. Hello!');
+  next();
+});
+
+app.use(function (req, res, next) {
+  console.log('2 I am next.');
+  next();
+});
+
+app.use(function(req, res, next) {
+  console.log('3 I am after next', req.method, req.url, req.user);
+  next();
+});
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   fs.readFile('./index.html', 'utf-8', function(err, data){
@@ -9,6 +34,10 @@ app.get('/', function(req, res) {
     res.set('Content-Type', 'text/html');
     res.send(data);
   });
+});
+
+app.get('/about', function(req, res) {
+  res.redirect('/bio.html');
 });
 
 var port = 3000;
